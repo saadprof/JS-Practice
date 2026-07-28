1ac3a22604580efeac385565f58d4f05a8677f2e
saadprof <saadprof101@gmail.com>
2026-07-28 14:33:07 +0600

    A conditional statement that checks a number if it is a positive, negative or zero.

---
+++ Conditional Statement/conditional.js
@@ -0,0 +1,35 @@
+//          ------ Basic (100) tasks ------
+
+// const sampleNum2 = 276;
+// const sampleNum3 = 421;
+// const sampleNum4 = 212;
+// const sampleNum5 = 190;
+// const sampleNum6 = 336;
+// const sampleNum7 = 80;
+// const sampleNum8 = 168;
+// const sampleNum9 = -64;
+// const sampleNum10 = 40;
+// const sampleNum11 = 108;
+// const sampleNum12 = -111;
+// const sampleNum13 = -138;
+// const sampleNum14 = 31;
+// const sampleNum15 = 416;
+// const sampleNum16 = -245;
+// const sampleNum17 = 174;
+// const sampleNum18 = -97;
+// const sampleNum19 = 283;
+// const sampleNum20 = 422;
+// const sampleNum21 = 385;
+// const sampleNum22 = 494;
+// const sampleNum23 = -57;
+// const sampleNum24 = 215;
+// const sampleNum25 = -91;
+// 1. Check if a number is positive, negative, or zero
+const sampleNum1 = 0;
+if(sampleNum1 >= 1){
+    console.log(`Number is a positive.`);
+} else if(sampleNum1 < 0){
+    console.log(`Number is a negative.`);
+} else{
+    console.log(`Number is zero.`);
+}
