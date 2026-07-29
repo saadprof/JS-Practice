b3eed3630a94b3a429c442f85d511f15ffa722d3
saadprof <saadprof101@gmail.com>
2026-07-29 20:25:42 +0600

    Conditional statemnt that checks if a number is divisible by 2 or not

--- Conditional Statement/conditional.js
+++ Conditional Statement/conditional.js
@@ -1,6 +1,5 @@
 //          ------ Basic (100) tasks ------
 
-// const sampleNum10 = 40;
 // const sampleNum11 = 108;
 // const sampleNum12 = -111;
 // const sampleNum13 = -138;
@@ -17,7 +16,7 @@
 // const sampleNum24 = 215;
 // const sampleNum25 = -91;
 
-// 1. 
+// 1.
 console.log("Task 1: Check if a number is positive, negative, or zero");
 const sampleNum1 = 158; //change the number to check if the condition works
 if (sampleNum1 >= 1) {
@@ -29,7 +28,7 @@ if (sampleNum1 >= 1) {
 }
 console.log("------------------------------------");
 
-// 2. 
+// 2.
 console.log("Task 2: Check if a number is even or odd");
 const sampleNum2 = 277;
 if (sampleNum2 % 2 === 0) {
@@ -131,4 +130,12 @@ if (sampleString1.length === 0) {
 }
 console.log("-----------------------------------");
 
-console.log();
+// 10
+console.log("Task 10: Check if a number is divisible by 2");
+const sampleNum10 = 40;
+if (sampleNum10 % 2 === 0) {
+    console.log(`The number ${sampleNum10} is absolutely divisible by 2.`);
+} else {
+    console.log(`${sampleNum10} can't be divided by 2 cause it's a odd one.`);
+}
+console.log("-----------------------------------");
